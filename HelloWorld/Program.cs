24d5d8a1679d7b01d27f39d21202efb83886392b
Ivan A Rodriguez <ivancalvo91@gmail.com>
2025-04-10 11:03:44 -0400

    Practice: Literal variables - char & int

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,3 +3,15 @@
 Console.Write("Congratulations!");
 Console.Write(" ");
 Console.Write("You wrote your first lines of code.");
+
+// In the case above, Console is a class, which have different
+//  methods inside, one of those is Write. A method starts with
+// capital letter, uses parentheses, and each statement ends with ";".
+
+// 2- Literal Variables
+
+// Character - char
+Console.WriteLine('q');
+
+// Integer - int
+Console.WriteLine(123);
